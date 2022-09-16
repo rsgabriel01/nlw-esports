6a42218244144155bddd920055d8fd3a1a78da3f
Gabriel Rodrigues <rsgabriel01@gmail.com>
2022-09-16 13:32:40 -0300

    finalizado layout landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,5 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
 import './styles/main.css'
 
 import logoImg from './assets/logo-nlw-esports.svg'
@@ -16,29 +18,78 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="" className="relative">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-1.png" alt="" />
 
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient">
-            <strong className="font-bold">League Of Legends</strong>
-            <span>4 anúncios</span>
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white">League Of Legends</strong>
+            <span className="text-zinc-300 text-sm block ">4 anúncios</span>
           </div>
         </a>
-        <a href="" className="relative">
+
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-2.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white">Dota 2</strong>
+            <span className="text-zinc-300 text-sm block ">4 anúncios</span>
+          </div>
         </a>
-        <a href="" className="relative">
+
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-3.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white">Counter Strike</strong>
+            <span className="text-zinc-300 text-sm block ">4 anúncios</span>
+          </div>
         </a>
-        <a href="" className="relative">
+
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-4.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white">Apex Legends</strong>
+            <span className="text-zinc-300 text-sm block ">4 anúncios</span>
+          </div>
         </a>
-        <a href="" className="relative">
+
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-5.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white">Fortnite</strong>
+            <span className="text-zinc-300 text-sm block ">4 anúncios</span>
+          </div>
         </a>
-        <a href="" className="relative">
+
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-6.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white">World of Warcraft</strong>
+            <span className="text-zinc-300 text-sm block ">4 anúncios</span>
+          </div>
         </a>
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">
+              Não encontrou o seu duo?
+            </strong>
+
+            <span className="text-zinc-400">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded">
+            <MagnifyingGlassPlus size="24" />
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </div>
   )
